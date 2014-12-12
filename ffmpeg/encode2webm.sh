af467865dee95c5afc72a3b0c087d84f628b7b0e
Guido Masella <guido.masella@gmail.com>
2014-12-12 19:04:27 +0000

    experimental codecs in ffmpeg

--- ffmpeg/encode2webm.sh
+++ ffmpeg/encode2webm.sh
@@ -21,4 +21,4 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-ffmpeg -i "$1"  -codec:v libvpx -quality good -cpu-used 0 -b:v 600k -maxrate 600k -bufsize 1200k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k "$2.webm"
+ffmpeg -i "$1"  -codec:v libvpx -quality good -cpu-used 0 -b:v 600k -maxrate 600k -bufsize 1200k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k -strict -2 "$2.webm"
