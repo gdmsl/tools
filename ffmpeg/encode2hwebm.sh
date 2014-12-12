72a619105f8efa7e6b5f43cbb348dc74b62e2e47
Guido Masella <guido.masella@gmail.com>
2014-12-12 19:39:25 +0000

    using libvorbis instead of native vorbis

--- ffmpeg/encode2hwebm.sh
+++ ffmpeg/encode2hwebm.sh
@@ -21,4 +21,4 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-ffmpeg -i "$1" -codec:v libvpx -quality good -cpu-used 0 -b:v 1200k -maxrate 1200k -bufsize 2400k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k -strict -2 "$2.webm"
+ffmpeg -i "$1" -codec:v libvpx -quality good -cpu-used 0 -b:v 1200k -maxrate 1200k -bufsize 2400k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a libvorbis -b:a 128k -strict -2 "$2.webm"
