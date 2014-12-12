a7725b7304b6e8a0ac162dd897e256fec0c7536e
Guido Masella <guido.masella@gmail.com>
2014-12-12 18:56:09 +0000

    initial commit. added some ffmpeg utils

---
+++ ffmpeg/encode2hwebm.sh
@@ -0,0 +1,24 @@
+#!/bin/dash
+
+#
+# AUTHOR: Guido Masella <guido.masella@gmail.com>
+#
+
+echo "This script will encode a file to an high quality webm format"
+echo " -> High-quality SD video for archive/storage (PAL at 1.2Mbit/s)"
+echo ""
+echo "usage: encode2hwebm.sh inputfile outputfile"
+echo "       (output filename without webm extension)"
+echo ""
+
+if [ ! -f "$1" ]; then
+    echo "ERROR: no such input file" >&2
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "ERROR: you need to provide an output filename (without extension)" >&2
+    exit 1
+fi
+
+ffmpeg -i "$1" -codec:v libvpx -quality good -cpu-used 0 -b:v 1200k -maxrate 1200k -bufsize 2400k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k "$2.webm"
---
+++ ffmpeg/encode2webm.sh
@@ -0,0 +1,24 @@
+#!/bin/dash
+
+#
+# AUTHOR: Guido Masella <guido.masella@gmail.com>
+#
+
+echo "This script will encode a file to a standard web quality webm format"
+echo " -> Standard web video (480p at 600kbit/s)"
+echo ""
+echo "usage: encode2webm.sh inputfile outputfile"
+echo "       (output filename without webm extension)"
+echo ""
+
+if [ ! -f "$1" ]; then
+    echo "ERROR: no such input file" >&2
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "ERROR: you need to provide an output filename (without extension)" >&2
+    exit 1
+fi
+
+ffmpeg -i "$1"  -codec:v libvpx -quality good -cpu-used 0 -b:v 600k -maxrate 600k -bufsize 1200k -qmin 10 -qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k "$2.webm"
